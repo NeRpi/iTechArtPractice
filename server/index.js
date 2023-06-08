c5f31b9441915d813049185f4e3f6f3de852f908
NeRpi <dmitrykruk12@gmail.com>
2023-06-08 15:16:24 +0300

    connect to pg

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+const sequelize = require("./db");
 
 const PORT = process.env.PORT || 5000;
 const app = express();
@@ -8,8 +9,14 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  return res.json("Hi");
-});
+const start = async () => {
+  try {
+    await sequelize.authenticate();
+    await sequelize.sync();
+    app.listen(PORT, () => console.log(`Server started on port: ${PORT}`));
+  } catch (e) {
+    console.log(e);
+  }
+};
 
-app.listen(PORT, () => console.log(`Server started on: ${PORT}`));
+start();
