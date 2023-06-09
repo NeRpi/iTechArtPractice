b7082f00335fb5ebd54807d6197067b41594680b
NeRpi <dmitrykruk12@gmail.com>
2023-06-09 12:43:11 +0300

    create user entity

---
+++ server/db/entitys/user.entity.js
@@ -0,0 +1,13 @@
+import { DataTypes } from "sequelize";
+import dbConnector from "../dbConnector.js";
+
+const UserEntity = dbConnector.define("user", {
+  id: { type: DataTypes.INTEGER, autoIncrement: true, primaryKey: true },
+  name: { type: DataTypes.STRING, allowNull: true, maxLength: 30 },
+  surname: { type: DataTypes.STRING, allowNull: true, maxLength: 30 },
+  DoB: { type: DataTypes.DATE },
+  email: { type: DataTypes.STRING },
+  password: { type: DataTypes.STRING, minLength: 15, maxLength: 30 },
+});
+
+export default UserEntity;
