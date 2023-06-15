969ca83de95b68268ee668d327950323b6f25734
NeRpi <dmitrykruk12@gmail.com>
2023-06-15 13:22:32 +0300

    deleting the last maxAge

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -40,10 +40,7 @@ class AuthController {
     try {
       const { refreshToken } = req.cookies;
       const userData = await this._authService.refresh(refreshToken);
-      res.cookie("refreshToken", userData.refreshToken, {
-        maxAge: 30 * 24 * 60 * 60 * 1000,
-        httpOnly: true,
-      });
+      res.cookie("refreshToken", userData.refreshToken, { httpOnly: true });
       return res.json(userData);
     } catch (e) {
       next(e);
