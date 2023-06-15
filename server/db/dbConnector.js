ba9c0fe303db32614a7b39664efa88cfca9ab5a0
NeRpi <dmitrykruk12@gmail.com>
2023-06-15 20:01:41 +0300

    adding docker

--- server/db/dbConnector.js
+++ server/db/dbConnector.js
@@ -1,7 +1,7 @@
 import { Sequelize } from "sequelize";
 
 export default new Sequelize(
-  process.env.POSTGRES_DB,
+  process.env.POSTGRES_NAME,
   process.env.POSTGRES_USER,
   process.env.POSTGRES_PASSWORD,
   {
