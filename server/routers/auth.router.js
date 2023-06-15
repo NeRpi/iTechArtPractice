5b2c7403f06141a32825b07f132dc3f59405c91a
NeRpi <dmitrykruk12@gmail.com>
2023-06-15 13:24:51 +0300

    refresh from get to post

--- server/routers/auth.router.js
+++ server/routers/auth.router.js
@@ -6,6 +6,6 @@ const router = new Router();
 router.post("/registration", authController.registration);
 router.post("/login", authController.login);
 router.post("/logout", authController.logout);
-router.get("/refresh", authController.refresh);
+router.post("/refresh", authController.refresh);
 
 export default router;
