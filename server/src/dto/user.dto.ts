3df4cd629c213eaf5346032d713eae76b92d11e0
NeRpi <dmitrykruk12@gmail.com>
2023-06-21 21:24:40 +0300

    removed 'public' modifier

--- server/src/dto/user.dto.ts
+++ server/src/dto/user.dto.ts
@@ -1,11 +1,11 @@
 export class UserDto {
-  public id?: string;
-  public name?: string;
-  public surname?: string;
-  public DoB?: string;
-  public email: string;
-  public password: string;
-  public roleId?: string;
+  id?: string;
+  name?: string;
+  surname?: string;
+  DoB?: string;
+  email: string;
+  password: string;
+  roleId?: string;
 
   constructor(data: any) {
     ({
