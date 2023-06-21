d0c6b230a473387bb64194c1d7555033944bd2a3
NeRpi <dmitrykruk12@gmail.com>
2023-06-21 11:47:15 +0300

    fixed minio.service.ts

--- server/src/services/minio.service.ts
+++ server/src/services/minio.service.ts
@@ -1,18 +1,16 @@
 import { Client } from "minio";
 
-const minioClient = new Client({
-  endPoint: "localhost",
-  port: 9000,
-  useSSL: false,
-  accessKey: "admin",
-  secretKey: "minio123",
-});
-
 export default class MinioService {
   private minioClient;
 
   constructor() {
-    this.minioClient = minioClient;
+    this.minioClient = new Client({
+      endPoint: process.env.MINIO_ENDPOINT!,
+      port: parseInt(process.env.MINIO_PORT!),
+      useSSL: false,
+      accessKey: process.env.MINIO_ACCESS_KEY!,
+      secretKey: process.env.MINIO_SECRETE_KEY!,
+    });
   }
 
   uploadFile(file: string, metaData: object) {
