042e386d2a4253a306057d452f6d3c1a419157b4
NeRpi <dmitrykruk12@gmail.com>
2023-06-23 15:41:23 +0300

    tasks

---
+++ tasks/228.js
@@ -0,0 +1,33 @@
+// https://leetcode.com/problems/summary-ranges/
+
+// You are given a sorted unique integer array nums.
+//
+// A range [a,b] is the set of all integers from a to b (inclusive).
+//
+// Return the smallest sorted list of ranges that cover all the numbers in the array exactly.
+// That is, each element of nums is covered by exactly one of the ranges, and there is no integer
+// x such that x is in one of the ranges but not in nums.
+//
+// Each range [a,b] in the list should be output as:
+//
+//      "a->b" if a != b
+//      "a" if a == b
+
+
+/**
+ * @param {number[]} nums
+ * @return {string[]}
+ */
+var summaryRanges = function (nums) {
+    let result = [];
+    let f = nums.at(0);
+    for (let i = 0; i < nums.length; i++) {
+        if (nums.at(i) !== nums.at(i + 1) - 1) {
+            if (f !== nums.at(i)) result.push(`${f}->${nums.at(i)}`)
+            else result.push(`${nums.at(i)}`);
+            f = nums.at(i + 1);
+        }
+    }
+
+    return result;
+};
