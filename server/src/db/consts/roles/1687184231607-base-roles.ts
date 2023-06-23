7b1c7a7dfba493b8e758f3197d71e09cb72454d7
NeRpi <dmitrykruk12@gmail.com>
2023-06-23 13:01:37 +0300

    a little refactoring

--- server/src/db/consts/roles/1687184231607-base-roles.ts
+++ server/src/db/consts/roles/1687184231607-base-roles.ts
@@ -4,9 +4,7 @@ export class BaseRoles1687184231607 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(`INSERT INTO "roles" ("role") VALUES('user')`);
     await queryRunner.query(`INSERT INTO "roles" ("role") VALUES('admin')`);
-    await queryRunner.query(
-      `INSERT INTO "roles" ("role") VALUES('SuperAdmin')`
-    );
+    await queryRunner.query(`INSERT INTO "roles" ("role") VALUES('SuperAdmin')`);
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
