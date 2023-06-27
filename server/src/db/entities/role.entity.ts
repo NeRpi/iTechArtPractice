cad5afea813892e8e84aae95c6a8e7bb6646a061
NeRpi <dmitrykruk12@gmail.com>
2023-06-27 19:51:03 +0300

    fix role.repo.ts

--- server/src/db/entities/role.entity.ts
+++ server/src/db/entities/role.entity.ts
@@ -1,12 +1,12 @@
 import { Entity, DeleteDateColumn, Column, OneToMany } from "typeorm";
 import { BaseEntity } from "./base.entity.ts";
 import { UserEntity } from "./user.entity.ts";
-import { Roles } from "../enums/role.enum.js";
+import { Roles } from "../enums/role.enum.ts";
 
 @Entity("roles")
 export class RoleEntity extends BaseEntity {
   @Column({ type: "enum", enum: Roles })
-  role: Roles;
+  role: string | Roles;
 
   @DeleteDateColumn()
   deleted_at: Date;
